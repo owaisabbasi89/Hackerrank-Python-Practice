7cbdd1a7e820b45d1cf59951fa8781276772296d
Owais Abbasi <owais.abbasi9@gmail.com>
2024-03-03 01:33:56 +0500

    practiced string manipulation etc.

---
+++ 014-designer-door-mat.py
@@ -0,0 +1,67 @@
+'''
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be X. ( is an odd natural number, and  is  times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+Input Format
+
+A single line containing the space separated values of  and .
+
+Constraints
+
+Output Format
+
+Output the design pattern.
+
+Sample Input
+
+9 27
+Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = (input()).split()
+n = int(a[0])
+m = int(a[1])
+
+for i in range((n-1)//2):
+    print((('.|.')*((i*2)+1)).center(m, '-'))
+
+print('WELCOME'.center(m, '-'))
+
+for i in range((n-1)//2):
+    print((('.|.')*((n-2)+(i*-2))).center(m, '-'))
