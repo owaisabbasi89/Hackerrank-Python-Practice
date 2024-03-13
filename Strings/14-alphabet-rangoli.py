3614b795b62986c6fa65405d0ae2f5a84fa13c0f
Owais Abbasi <owais.abbasi9@gmail.com>
2024-03-13 20:21:43 +0500

    alphabet rangoli

---
+++ Strings/14-alphabet-rangoli.py
@@ -0,0 +1,127 @@
+'''
+
+You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+
+Different sizes of alphabet rangoli are shown below:
+
+#size 3
+
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+
+#size 5
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+#size 10
+
+------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+The center of the rangoli has the first alphabet letter a, and the boundary has the  alphabet letter (in alphabetical order).
+
+Function Description
+
+Complete the rangoli function in the editor below.
+
+rangoli has the following parameters:
+
+int size: the size of the rangoli
+Returns
+
+string: a single string made up of each of the lines of the rangoli separated by a newline character (\n)
+Input Format
+
+Only one line of input containing , the size of the rangoli.
+
+Constraints
+
+
+Sample Input
+
+5
+Sample Output
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+'''
+
+def print_rangoli(size):
+
+    # your code goes here
+    ls = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    lrwidth = (size+size-1+size+size-3)//2
+
+    # ------------Top Half------------
+
+    for i in range(size):
+
+        left = ""
+
+        for j in reversed(ls[size-i:size]):
+            left += f"{j}-"
+            
+        right = ""
+        
+        for k in ls[size-i:size]:
+            right += f"-{k}"
+            
+            
+        print(left.rjust(lrwidth,'-')+(ls[size-1-i]).center(1, '0')+right.ljust(lrwidth, '-'))
+        
+    # ----------Bottom half-----------
+    for i in range(size-1):
+
+        left = ""
+
+        for j in reversed(ls[i+2:size]):
+            left += f"{j}-"
+            
+        right = ""
+        
+        for k in ls[i+2:size]:
+            right += f"-{k}"
+            
+            
+        print(left.rjust(lrwidth,'-')+(ls[i+1]).center(1, '0')+right.ljust(lrwidth, '-'))
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
